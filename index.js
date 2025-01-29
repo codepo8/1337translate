3348f33f6bc828d3c863f1ceb83df7efff1c3402
Christian Heilmann <chris.heilmann@gmail.com>
2025-01-29 06:43:59 +0100

    cosmetics

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const leettranslate = () => {};
 leettranslate.fullmap = {
-	'e': '3',
-	'l': '1',
-	'o': '0',
-	't': '7',
-	'a': '4',
-	's': '5',
-	'b': '8',
-	'g': '9',
-	'i': '!',
-	'z': '2'
+	'e': '3','l': '1','o': '0','t': '7','a': '4','s': '5',
+	'b': '8','g': '9','i': '!','z': '2'
 };
 leettranslate.lightmap = {
-	'e': '3',
-	'l': '1',
-	'o': '0',
-	't': '7',
-	'a': '4',
-	's': '5',
+	'e': '3','l': '1','o': '0','t': '7','a': '4','s': '5'
 };
-leettranslate.toleet = (text, modes={light:false}) =>{
+leettranslate.toleet = (text, modes = {light:false}) =>{
 	let map = modes.light ? leettranslate.lightmap : leettranslate.fullmap;
 	return text.split('').map(char => {
-		if (map[char.toLowerCase()]) {
-			return map[char.toLowerCase()];
+		let smol = char.toLowerCase();
+		if (map[smol]) {
+			return map[smol];
 		}
 		return char;
 	}).join('');
 }   
-leettranslate.totext = (text, modes={light:false}) => {
+leettranslate.totext = (text, modes = {light:false}) => {
 	let map = modes.light ? leettranslate.lightmap : leettranslate.fullmap;
 	return text.split('').map(char => {
 		for (let key in map) {
